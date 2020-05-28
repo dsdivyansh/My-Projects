613680b1b9c0bdf5926152ade763d439e6af35ab
dsdivyansh <65468199+dsdivyansh@users.noreply.github.com>
2020-05-28 21:43:29 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -19,19 +19,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-# class Comment(models.Model):
-#     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
-#     author = models.CharField(max_length=200)
-#     text = models.TextField()
-#     created_date = models.DateTimeField(default=timezone.now)
-#     approved_comment = models.BooleanField(default=False)
-#
-#
-#     class Meta:
-#         ordering = ['created_date']
-#
-#     def __str__(self):
-#         return 'Comment {} by {}'.format(self.body,self.name)
+
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
